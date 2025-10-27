c32497a63f55d45e49bc7ca406d2d6abfdc8b4d5
Doctorpy <djakpo.gado20@gmail.com>
2025-10-27 16:55:34 +0000

    before modifie

--- run_dockers.sh
+++ run_dockers.sh
@@ -4,21 +4,21 @@ echo "Démarrage des conteneurs DHIS2..."
 
 # Démarrer le conteneur dhis2_20
 echo "Démarrage de dhis2 20"
-cd ~/Applications/Dhis2/dhis2_20
+cd ~/Applications/dhis2/dhis2_20
 docker compose down
 docker compose up -d
 
 # Démarrer le conteneur dhis2_21
-echo "Démarrage de dhis2 21..."
-cd ~/Applications/Dhis2/dhis2_21
-docker compose down
-docker compose up -d
+# echo "Démarrage de dhis2 21..."
+# cd ~/Applications/Dhis2/dhis2_21
+# docker compose down
+# docker compose up -d
 
 # Démarrer le conteneur dhis2_22
-echo "Démarrage de dhis2 22..."
-cd ~/Applications/Dhis2/dhis2_22
-docker compose down
-docker compose up -d
+# echo "Démarrage de dhis2 22..."
+# cd ~/Applications/Dhis2/dhis2_22
+# docker compose down
+# docker compose up -d
 
-echo "Tous les conteneurs DHIS2 ont été démarrés."
+# echo "Tous les conteneurs DHIS2 ont été démarrés."
 
